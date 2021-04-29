9984999dd942529220be51ca51ad5f84a4f35bdb
aitaj <aytajez@code.edu.az>
2021-04-29 04:40:05 +0400

    navbar structural finished homepage finished

--- p-511FronEndProject/homepage/assets/js/script.js
+++ p-511FronEndProject/homepage/assets/js/script.js
@@ -24,11 +24,21 @@ listingUlLi.forEach((item) => {
   });
 });
 
+
+
+//navbar started
+
+let barIcon=document.querySelector(".nav-bar-icon");
+let navWrapper=document.querySelector(".nav-main-ul")
+barIcon.addEventListener("click", function(e){
+ navWrapper.parentNode.style.display="inline-block";
+ navWrapper.style.height=`${window.screen.height}px`;
+})
+
+
 //#house-in,#latest-properties layer
 
-let cardHeaders = document.querySelectorAll(
-  " .house-in-card .img-wrapper"
-);
+let cardHeaders = document.querySelectorAll(" .house-in-card .img-wrapper");
 
 cardHeaders.forEach((item) => {
   item.addEventListener("mouseenter", function () {
@@ -39,76 +49,69 @@ cardHeaders.forEach((item) => {
   });
 });
 
-
 //lifestyle item layer
-let lifestyleItems=document.querySelectorAll("#lifestyles .item");
+
+
+let lifestyleItems = document.querySelectorAll("#lifestyles .item");
 
 lifestyleItems.forEach((item) => {
-  item.addEventListener("mouseenter",function(){
+  item.addEventListener("mouseenter", function () {
     item.classList.add("border");
     item.children[0].children[1].classList.remove("d-none");
     item.children[0].children[0].classList.add("d-none");
-  })
-  item.addEventListener("mouseleave",function(){
+  });
+  item.addEventListener("mouseleave", function () {
     item.classList.remove("border");
     item.children[0].children[1].classList.add("d-none");
     item.children[0].children[0].classList.remove("d-none");
-  })
-})
+  });
+});
 
-
-//sliders 
+//sliders carousels--started//////////////////
 
 $(document).ready(function () {
-//#home-id slider
-$(".home-id-slider").owlCarousel({
-  loop: true,
-  margin: 30,
+//entry slider
+
+$(".entry-slider ").owlCarousel({
+  margin: 20,
   responsiveClass: true,
-  dots:true,
-  nav:false,
+  dots: false,
+  nav: false,
   responsive: {
     0: {
       items: 1,
     },
     576: {
-      items: 2,
+      items: 1,
     },
     992: {
-      items: 4,
+      items: 1,
     },
   },
 });
 
+var owl = $('.entry-slider');
+owl.owlCarousel();
+// Go to the next item
+$('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
 
-//#comments slider
-$(".comment-slider").owlCarousel({
-  
-  margin: 30,
-  responsiveClass: true,
-  dots:true,
-  nav:false,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    576: {
-      items: 2,
-    },
-    992: {
-      items: 3,
-    },
-  },
-}); 
 
-//#news-articles slider
 
-  $(".news-articles-slider").owlCarousel({
-  
+  //#home-id slider
+  $(".home-id-slider").owlCarousel({
+    loop: true,
     margin: 30,
     responsiveClass: true,
-    dots:false,
-    nav:false,
+    dots: true,
+    nav: false,
     responsive: {
       0: {
         items: 1,
@@ -121,5 +124,93 @@ $(".comment-slider").owlCarousel({
       },
     },
   });
+
+  //#comments slider
+  $(".comment-slider").owlCarousel({
+    margin: 30,
+    responsiveClass: true,
+    dots: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+
+  //#news-articles slider
+
+  $(".news-articles-slider").owlCarousel({
+    margin: 20,
+    responsiveClass: true,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      992: {
+        items: 4,
+      },
+    },
+  });
+  //#testimonial slider
+
+  $(".testimonial-slider ").owlCarousel({
+    margin: 20,
+    responsiveClass: true,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      576: {
+        items: 4,
+      },
+      992: {
+        items: 6,
+      },
+    },
+  });
 });
 
+//fixed-btn
+
+let btnFixed = document.querySelector(".sticky-btn");
+
+const scrollFunc = () => {
+
+  let y = window.scrollY;
+
+   if (y > 230) {
+     console.log(y);
+    btnFixed.className = "active sticky-btn";
+  } else {
+    btnFixed.className = "hide sticky-btn";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
+};
+btnFixed.onclick = function (e) {
+  e.preventDefault();
+  scrollToTop();
+};

dae0092c9fd10ac929122d2bd1e1cc1ab51138d8
aitaj <aytajez@code.edu.az>
2021-05-02 23:05:12 +0400

    blog page is finished

--- p-511FronEndProject/assets/js/script.js
+++ p-511FronEndProject/assets/js/script.js
@@ -323,8 +323,7 @@ $(document).ready(function () {
   // #video-pop-up section
   $("#video-pop-up .play-btn").fancybox({});
 
-// #about us slide images
-
+  // #about us slide images
 
   $('[data-fancybox="images"]').fancybox({
     buttons: ["slideShow", "share", "zoom", "fullScreen", "close"],
@@ -348,3 +347,21 @@ leaders.forEach((item) => {
   });
 });
 
+//blog section started
+let sideInfo = document.querySelector("#blogs .side-info");
+let footer = document.querySelector("footer");
+let diffScroll = document.body.offsetHeight - footer.offsetHeight;
+window.addEventListener("scroll", function (e) {
+  if (window.scrollY > 923&&window.scrollY < 7000 ) {
+    sideInfo.style.position = "fixed";
+    sideInfo.style.bottom = "0";
+    sideInfo.style.width = `${sideInfo.parentNode.offsetWidth}px`;
+  } 
+  else if(window.scrollY > 7000 ){
+    sideInfo.style.position = "absolute";
+    sideInfo.style.bottom = "0";
+  }
+  else{
+    sideInfo.style.position = "static";
+  }
+});
